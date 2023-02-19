0b3242922166450e3f96c396d6ea8fa3e0b07398
Aysllansw <aysllansouzamachado@gmail.com>
2023-02-19 17:18:41 -0300

    pagina-web

---
+++ index.js
@@ -0,0 +1,10 @@
+/*const btn = document.querySelector("span");
+
+btn = addEventListener("click",function(){
+    this.alert("Esta página está em manutenção no momento!");
+}
+);*/
+function btn_menu(){
+    alert("Esta página está em manutenção.");
+}
+
